b7952453ef618fd8990f1cdd63c55564692c58e6
andrei-trifonov <rsanddrs-company@yandex.ru>
2020-09-05 15:11:06 +0300

    init

---
+++ src/Script1.java
@@ -0,0 +1,31 @@
+public class Script1 {
+    int number;
+    double num = 33.33;
+    String text;
+    char a;
+    int Sum (int a, int b)
+    {
+        return a+b;
+    }
+    void printing (String asd ){
+        System.out.println(asd);
+    }
+}
+class App{
+    int var;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Script1 dog = new Script1();
+        Script1 cat = new Script1();
+        System.out.println("Dog " + dog.number);
+        System.out.println("Cat " + cat.number);
+    }
+}
+
+    class son extends App
+    {
+    void func(int  var) {
+
+        this.var = var;
+    }
+    }
